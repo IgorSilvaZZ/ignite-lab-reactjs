dfe30e7a59e46b054af0fcfbe6d4c5488762f8cf
IgorSilvaZZ <igor06silva12@gmail.com>
2022-08-17 21:50:05 -0300

    Class 2 done

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,25 +1,49 @@
-import { CheckCircle } from "phosphor-react";
+import { CheckCircle, Lock } from "phosphor-react";
+import { isPast, format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
-export function Lesson() {
+interface LessonProps {
+  title: string;
+  slug: string;
+  availableAt: Date;
+  type: "live" | "class";
+}
+
+export function Lesson(props: LessonProps) {
+  const isLessonAvailable = isPast(props.availableAt);
+
+  const availableDateFormatted = format(
+    props.availableAt,
+    "EEEE' º 'd' de 'MMMM' º 'k'h'mm",
+    {
+      locale: ptBR,
+    }
+  );
+
   return (
     <a href='#'>
-      <span className='text-gray-300'>Terça • 22 de junho • 19h00</span>
+      <span className='text-gray-300'>{availableDateFormatted}</span>
 
       <div className='rounded border border-gray-500 p-4 mt-2'>
         <header className='flex items-center justify-between'>
-          <span className='text-sm text-blue-500 font-medium flex items-center gap-2'>
-            <CheckCircle size={20} />
-            Conteudo Liberado
-          </span>
+          {isLessonAvailable ? (
+            <span className='text-sm text-blue-500 font-medium flex items-center gap-2'>
+              <CheckCircle size={20} />
+              Conteudo Liberado
+            </span>
+          ) : (
+            <span className='text-sm text-orange-500 font-medium flex items-center gap-2'>
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
 
           <span className='text-xs rounded py-[0.125rem] px-2 text-white border border-green font-bold'>
-            AO VIVO
+            {props.type === "live" ? "AO VIVO" : "AULA PRATICA"}
           </span>
         </header>
 
-        <strong className='text-gray-200 mt-5 block'>
-          Aula 02 - Titulo aula ignite Lab
-        </strong>
+        <strong className='text-gray-200 mt-5 block'>{props.title}</strong>
       </div>
     </a>
   );
